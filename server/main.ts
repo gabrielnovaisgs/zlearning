e7f309bb0a2aebe35af6d68802a950faea13faf2
gabrielnovaisgs <gabrielnovaisgs@gmail.com>
2026-03-19 21:01:58 -0300

    feat: implement Swagger for API documentation

--- server/main.ts
+++ server/main.ts
@@ -1,15 +1,25 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
 
+  const config = new DocumentBuilder()
+    .setTitle('ZLearning API')
+    .setDescription('The ZLearning API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
   const port = 3000;
   await app.listen(port);
   console.log(`API running at http://localhost:${port}`);
+  console.log(`Swagger docs at http://localhost:${port}/api/docs`);
 }
 
 bootstrap();
